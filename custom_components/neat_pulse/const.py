fda94c70dc52ee87776ca92677c48a255025971f
eisenman <eisenman@users.noreply.github.com>
2024-10-17 22:14:32 -0700

    HACS folder path

---
+++ custom_components/neat_pulse/const.py
@@ -0,0 +1,10 @@
+# const.py
+
+DOMAIN = "neat_pulse"
+
+CONF_ACCESS_TOKEN = "access_token"
+CONF_ORGANIZATION_ID = "organization_id"
+CONF_ENDPOINT_ID = "endpoint_id"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+DEFAULT_SCAN_INTERVAL = .10  # in minutes
