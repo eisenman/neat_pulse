62218085112fe7785fef976cf5324a470d1294c4
eisenman <eisenman@users.noreply.github.com>
2024-10-19 21:41:08 -0700

    Update const.py

--- custom_components/neat_pulse/const.py
+++ custom_components/neat_pulse/const.py
@@ -2,9 +2,9 @@
 
 DOMAIN = "neat_pulse"
 
-CONF_ACCESS_TOKEN = "API Key"
-CONF_ORGANIZATION_ID = "Organization ID"
-CONF_ENDPOINT_ID = "Device ID"
+CONF_ACCESS_TOKEN = "access_token"
+CONF_ORGANIZATION_ID = "organization_id"
+CONF_ENDPOINT_ID = "endpoint_id"
 CONF_SCAN_INTERVAL = "scan_interval"
 
 DEFAULT_SCAN_INTERVAL = .10  # in minutes
